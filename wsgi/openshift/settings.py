eba7479ddbcbaed044d229948ea3b574690597ef
Andrew <andrew@debian.vmware-test>
2014-10-04 14:50:08 -0400

    Added ALLOWED_HOSTS. Removed logging.

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -16,6 +16,12 @@ else:
     DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+
+if DEBUG:
+    ALLOWED_HOSTS=[]
+else:
+    ALLOWED_HOSTS=['*']
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -156,25 +162,3 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
