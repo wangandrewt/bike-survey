fa2846885e6439aaefa6dd727325134d5f11f06d
Andrew <andrew@debian.vmware-test>
2014-10-04 20:53:14 -0400

    Changed /survey/ to /bikesurvey/ and added "record" view.

--- wsgi/openshift/bikesurvey/urls.py
+++ wsgi/openshift/bikesurvey/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, url
 from bikesurvey import views
 
 urlpatterns = patterns('',
-    # ex: /survey/
+    # ex: /bikesurvey/
     url(r'^$', views.index, name='index'),
-    # ex: /survey/5/
+    # ex: /bikesurvey/5/
     url(r'^(?P<surveyInstance_id>\d+)/$', views.detail, name='detail'),
-    # ex: /survey/5/record/
+    # ex: /bikesurvey/5/record/
     url(r'^(?P<surveyInstance_id>\d+)/record/$', views.record, name='record'),
 )
--- wsgi/openshift/bikesurvey/views.py
+++ wsgi/openshift/bikesurvey/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
-from bikesurvey.models import SurveyInstance
+from bikesurvey.models import SurveyInstance, Biker
 
 
 def index(request):
@@ -14,5 +15,21 @@ def detail(request, surveyInstance_id):
     surveyInstance = get_object_or_404(SurveyInstance, pk=surveyInstance_id)
     return render(request, 'bikesurvey/detail.html', {'surveyInstance': surveyInstance})
 
+
 def record(request, surveyInstance_id):
-    return HttpResponse("You're recording biker information on surveyInstance %s." % surveyInstance_id)
+    p = get_object_or_404(SurveyInstance, pk=surveyInstance_id)
+    try:
+        selected_choice = p.biker_set.get(pk=request.POST['biker'])
+    except (KeyError, Biker.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'bikesurvey/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.count += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('bikesurvey:detail', args=(p.id,)))
