aee64b6394b83f2b56dff3556136a318224f945c
Andrew <andrew@debian.vmware-test>
2014-12-16 22:16:19 -0500

    Change location to tuple.

--- wsgi/openshift/bikesurvey/models.py
+++ wsgi/openshift/bikesurvey/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class SurveyInstance(models.Model):
+    LOCATION_CHOICES=(
+        ('Regents Drive @ Rt. 1', 'Regents Drive @ Rt. 1'),
+        ('Mowatt Lane Garage Exit @ Knox Rd.', 'Mowatt Lane Garage Exit @ Knox Rd.'),
+        ('Mall @ Woods Hall', 'Mall @ Woods Hall'),
+        ('Regents Dr. @ Main Administration', 'Regents Dr. @ Main Administration'),
+        ('Campus Dr. @ Paint Branch Dr.', 'Campus Dr. @ Paint Branch Dr.'),
+        ('Campus Dr. @ The Stamp', 'Campus Dr. @ The Stamp'),
+        ('Regents Drive @ Stadium Drive', 'Regents Drive @ Stadium Drive'),
+        ('Paint Branch Drive @ Lot 11b', 'Paint Branch Drive @ Lot 11b'),
+        ('Campus Drive @ Adelphi Road', 'Campus Drive @ Adelphi Road'),
+    )
     name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, choices=LOCATION_CHOICES)
     date = models.DateField()
     def __unicode__(self):
         return self.name + " at " + self.location + " at " + self.date.strftime('%Y-%m-%d')
