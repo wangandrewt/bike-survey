6f2ca8ed3901352964d8bba69bba68d4620dcb29
Andrew T Wang <andrew@debian.vmware-test>
2015-01-15 22:03:38 -0500

    Change when AddBikerView displays a blank form
    Displays a blank form if GET request or if biker saved.
    Displays old form if form had errors (e.g, empty field).

--- bikesurvey/views.py
+++ bikesurvey/views.py
@@ -72,9 +72,11 @@ def AddBikerView(request, surveyInstance_id):
             biker.surveyInstance = surveyInstance
             biker.save()
             message = "Biker added."
-    
-    # create a blank form
-    form = forms.BikerForm()
+            # create a blank form
+            form = forms.BikerForm()
+    else:
+        # create a blank form
+        form = forms.BikerForm()
 
     return render(request, "bikesurvey/detail.html", {
         'form': form,
